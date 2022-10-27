e2ce6c4e0a15ceecdece5632fc6f50b8b308d085
PayalKPI <payal.pitale@kpipartners.com>
2022-10-27 18:02:15 +0530

    commit  "27th oct"

---
+++ demo/demo/src/main/java/com/kpi/microservice/currencyexchangeexampleservice/demo/ExchangeValueRepo.java
@@ -0,0 +1,9 @@
+package com.kpi.microservice.currencyexchangeexampleservice.demo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepo extends JpaRepository<ExchangeValueEntity,Integer>{
+ExchangeValueEntity findByFromAndTo(String from,String to);
+}
+	
+
+
